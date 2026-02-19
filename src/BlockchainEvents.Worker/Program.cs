eb76d22d1475252ece1a25e27b740be72a7ea059
ItsDaveB <49551821+ItsDaveB@users.noreply.github.com>
2026-02-19 20:59:54 +0000

    feat: configure HTTP client defaults for SSL certificate validation
    - add custom validation for server certificates in HTTP client

--- src/BlockchainEvents.Worker/Program.cs
+++ src/BlockchainEvents.Worker/Program.cs
@@ -22,6 +22,16 @@ builder.Services.AddSingleton<ICheckpointService, DaprCheckpointService>();
 
 builder.Services.AddOgmiosServices();
 
+builder.Services.ConfigureHttpClientDefaults(clientBuilder =>
+{
+    clientBuilder.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = (_, _, _, sslPolicyErrors) =>
+            sslPolicyErrors is System.Net.Security.SslPolicyErrors.None
+                          or System.Net.Security.SslPolicyErrors.RemoteCertificateNameMismatch
+    });
+});
+
 builder.Services.AddSingleton<OgmiosChainSyncAdapter>();
 builder.Services.AddSingleton<IChainSyncService>(sp => sp.GetRequiredService<OgmiosChainSyncAdapter>());
 builder.Services.AddSingleton<Ogmios.Services.ChainSynchronization.IChainSynchronizationMessageHandlers>(
